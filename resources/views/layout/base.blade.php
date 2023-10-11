ada0ee4e573d56c894094dd94d05a36087f598d5
nikondi <nikondi@narod.ru>
2023-10-11 16:26:36 +0300

    Logo, favicon. loader update

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -7,10 +7,13 @@
     @section('head')
         @seo
         <!-- favicons Icons -->
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon-16x16.png"/>
-        <link rel="manifest" href="assets/images/favicons/site.webmanifest"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicons/apple-touch-icon.png') }}"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicons/favicon-32x32.png') }}"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicons/favicon-16x16.png') }}"/>
+        <link rel="manifest" href="{{ asset('assets/images/favicons/site.webmanifest') }}"/>
+        <link rel="mask-icon" href="{{ asset('assets/images/favicons/safari-pinned-tab.svg') }}" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
 
         <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap/css/bootstrap.min.css') }}"/>
         <link rel="stylesheet" href="{{ asset('assets/vendors/animate/animate.min.css') }}"/>
